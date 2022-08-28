6170bac7bf8f5a7113b30b0143884df67f0b384f
hane <adrplusrayane@hotmail.com>
2022-08-28 15:47:07 +0100

    test commit from IDE

--- demo1/src/main/java/com/example/demo/Student/StudentRepository.java
+++ demo1/src/main/java/com/example/demo/Student/StudentRepository.java
@@ -1,9 +1,19 @@
 package com.example.demo.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 // this interface is responsible for data access that is why we are using it
 // we want to use this interface inside our StudentService
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
+
+    @Query ("SELECT S FROM Student S WHERE S.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+
 }
--- demo1/src/main/java/com/example/demo/Student/StudentService.java
+++ demo1/src/main/java/com/example/demo/Student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 // for the server
 @Service  // @Service and @Component are exactly the same thing
@@ -23,6 +24,13 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
     }
 }
